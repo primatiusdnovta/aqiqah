02af0369f316c09296ed51e03962e6cb2238f461
herry88 <herry081288@gmail.com>
2018-01-25 23:21:38 +0700

    Tambah aksi modul

---
+++ administrator/modul/mod_modul/aksi_modul.php
@@ -0,0 +1,28 @@
+<?php 
+		if(empty($_SESSION['namauser']) AND empty($_SESSION['passuser'])){
+	echo "<link href=\"../../css/style.css\" rel=\"stylesheet\" type=\"text/css\"  media=\"all\" />
+		<!--start-wrap--->
+		<div class=\"wrap\">
+			<!---start-header---->
+				<div class=\"header\">
+					<div class=\"logo\">
+						<h1><a href=\"#\">ATTENTION !!!</a></h1>
+					</div>
+				</div>
+			<!---End-header---->
+			<!--start-content------>
+			<div class=\"content\">
+				<img src=\"images/error-img.png\" title=\"error\" />
+				<p><span><label>O</label>hh.....</span>Please Login, Before Access This Page !!!</p>
+				<a href=\"index.php\">Back To Home</a>
+				<div class=\"copy-right\">
+					<p>&copy; 2018 Ohh. All Rights Reserved</p>
+				</div>
+   			</div>
+			<!--End-Cotent------>
+		</div>
+		<!--End-wrap--->";
+}
+else{
+	
+}
